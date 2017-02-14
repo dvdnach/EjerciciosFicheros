7197f0364fbb5e0459933cdf80d4f2fefcdc020c
dvdnach <dvdnach@hotmail.com>
2017-02-14 12:23:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dm2/ejerciciosficheros/Ejercicio1.java
@@ -0,0 +1,48 @@
+package com.example.dm2.ejerciciosficheros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Ejercicio1 extends AppCompatActivity {
+
+    private TextView lbl3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio1);
+        lbl3=(TextView) findViewById(R.id.lbl3);
+    }
+
+    public void añadirinterno (View v)
+    {
+        
+    }
+    public void añadirexterno(View v)
+    {
+
+    }
+    public void leerinterno(View v)
+    {
+
+    }
+    public void leerexterno(View v)
+    {
+
+    }
+    public void leerrecurso(View v)
+    {
+
+    }
+    public void borrarinterno(View v)
+    {
+
+    }
+    public void borrarexterno(View v)
+    {
+
+    }
+
+}
